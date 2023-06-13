67c3ca8a19059309ae4197f258f7f61d8c15d8a1
Shiv Prasad <shivprasad@Shivs-MacBook-Pro.local>
2023-06-12 18:31:47 -0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Text,
+
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+    <Text>Hello World</Text>
+  );
+}
+
+
+
+export default App;
